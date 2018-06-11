f71c4d33c6e53f1d22b679b3c3fecabcfe679e83
nushilizzy <38955646+nushilizzy@users.noreply.github.com>
2018-06-10 17:19:27 -0700

    Add files via upload

---
+++ finalproject.c
@@ -0,0 +1,83 @@
+#include<stdio.h> 
+int qualitiesofnumber(int n)
+{ int t, reverse = 0;
+ 
+  t = n;
+ 
+  while (t != 0) {
+    reverse = reverse * 10;
+    reverse = reverse + t%10;
+    t = t/10;
+  }
+ 
+  if (n == reverse)
+    printf("The solution is a palindrome!\n");
+  else
+    printf("The solution is not a palindrome!\n");
+
+    if (n == 0.0)
+        printf("The solution is 0.\n");
+    else if (n<0.0)
+        printf("The solution is a negative number.\n");
+    else
+        printf("The solution is a positive number.\n");
+int c=2;
+ for ( c = 2 ; c <= n - 1 ; c++ )
+   {
+      if ( n%c == 0 )
+      {
+         printf("The solution isn't prime.\n");
+     break;
+      }
+   }
+   if ( c == n )
+      printf("The solution is prime.\n");
+ if (n%2 == 0)
+      printf("The solution is even.\n");
+   else
+      printf("The solution is odd.\n");
+ 
+return 0;
+}
+
+
+int main() 
+{
+    char operator;
+    int num1,num2;
+
+    printf("Enter an operator (+, -, *, /): ");
+    scanf("%c", &operator);
+
+    printf("Enter two numbers: ");
+    scanf("%d %d",&num1, &num2);
+
+    switch(operator)
+    {
+        case '+':
+            printf("%d + %d = %d\n",num1, num2, num1 + num2);
+            int b= num1 + num2;
+	    qualitiesofnumber(b);
+	    break;
+        case '-':
+            printf("%d - %d = %d\n",num1, num2, num1 - num2);
+	    int c= num1 - num2;
+	    qualitiesofnumber(c);
+	    break;
+        case '*':
+            printf("%d * %d = %d\n",num1, num2, num1 * num2);
+            int d= num1 * num2;
+            qualitiesofnumber(d);
+	    break;
+        case '/':
+            printf("%d / %d = %d\n", num1, num2, num1 / num2);
+            int e= num1 / num2;
+	    qualitiesofnumber(e);
+	    break;
+        default:
+            printf("Enter a valid operator..");
+    }    
+    return 0;
+}
+ 
+
